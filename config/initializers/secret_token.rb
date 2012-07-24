00f447aef507e3b98a511079a2ce23c08aac13c9
vijaya sekhar <m.vijaysekhar@gmail.com>
2012-07-24 11:03:58 +0530

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RorExpert::Application.config.secret_token = '277ad0765707d04a39d19f1a0e8f44b4b5e976cba155ce30fd279ddf1ba28b99c778a7c1586996fd172347caa4910e9ab097c6fc0664cba6efbd132db90e23af'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+RorExpert::Application.config.session_store :cookie_store, key: '_ror_expert_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# RorExpert::Application.config.session_store :active_record_store
