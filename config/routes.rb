075ceea2b3a48e8c622ba90476bd77b7affc6f80
vijayasekharreddy <m.vijaysekhar@gmail.com>
2013-10-11 17:45:20 +0530

    Adding before_filter for application.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ RorExpert::Application.routes.draw do
   devise_for :users
 
   devise_scope :user do
+      get "/logout" => "devise/sessions#destroy"
       root to: "devise/sessions#new"
   end 
 
